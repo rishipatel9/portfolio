010ffe45ddf68a65b0c7f8e883287a2535e120a0
rishipatel9 <rishipatel0826@gmail.com>
2024-08-18 13:08:03 +0530

    bug fixes

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,7 +2,11 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import React from 'react';
+
+// Explicitly type NextThemesProvider as a React component
+const TypedNextThemesProvider = NextThemesProvider as React.ComponentType<ThemeProviderProps>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return <TypedNextThemesProvider {...props}>{children}</TypedNextThemesProvider>;
 }
